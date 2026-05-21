1551ceacab1d919d87b6508d7b26a0cc5a31e71c
Jorge Sanchez <jorgeluissanchezbarreneche@gmail.com>
2026-05-20 23:55:56 -0500

    fix(metro): resolve @react-three/fiber native subpath issue in Metro configuration

--- metro.config.js
+++ metro.config.js
@@ -7,4 +7,16 @@ const config = getDefaultConfig(__dirname);
 config.resolver.unstable_enablePackageExports = true;
 config.resolver.unstable_conditionNames = ["browser", "require", "default"];
 
-module.exports = withNativeWind(config, { input: './src/global.css', inlineRem: 16 });
+const nativeWindConfig = withNativeWind(config, { input: './src/global.css', inlineRem: 16 });
+
+// @react-three/fiber has an empty "exports" map, so Metro won't resolve
+// its /native subpath when package-exports mode is on. Bypass it explicitly.
+const upstream = nativeWindConfig.resolver.resolveRequest;
+nativeWindConfig.resolver.resolveRequest = (ctx, moduleName, platform) => {
+  if (moduleName === '@react-three/fiber/native') {
+    return { filePath: require.resolve('@react-three/fiber/native'), type: 'sourceFile' };
+  }
+  return upstream ? upstream(ctx, moduleName, platform) : ctx.resolveRequest(ctx, moduleName, platform);
+};
+
+module.exports = nativeWindConfig;
