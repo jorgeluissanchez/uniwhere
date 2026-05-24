096907ee8bc3e91a9b9bf2901cceffe1429e8d2d
Flavio Andres Arregoces Mercado <flavioarregoces@gmail.com>
2026-05-24 16:07:10 -0500

    fix(login): remove layered waves from background

--- src/features/auth/presentation/screens/login-screen.tsx
+++ src/features/auth/presentation/screens/login-screen.tsx
@@ -1,4 +1,3 @@
-import { LAYERED_WAVES_SVG } from "@/assets/svgs/layeredWaves";
 import { STUDENTS_RAFIKI_SVG } from "@/assets/svgs/studentsRafiki";
 import { Button } from "@/core/components/ui/button";
 import { Text } from "@/core/components/ui/text";
@@ -6,20 +5,16 @@ import { LoginForm } from "@/features/auth/presentation/components/login-form";
 import { RelativePathString, useRouter } from "expo-router";
 import { ArrowLeft } from "lucide-react-native";
 import React from "react";
-import { Dimensions, View } from "react-native";
+import { View } from "react-native";
 import { SvgXml } from "react-native-svg";
 
 export default function LoginScreen() {
   const router = useRouter();
-  const { width } = Dimensions.get("window");
 
   return (
     <View className="flex-1 bg-white overflow-hidden">
       <View pointerEvents="none" className="absolute inset-0">
         <SvgXml xml={STUDENTS_RAFIKI_SVG} width={500} height={300} className="absolute -top-16 left-1/2 -ml-[250px] -z-10" />
-        <View className="absolute bottom-0 left-0">
-          <SvgXml xml={LAYERED_WAVES_SVG} width={width} height={140} preserveAspectRatio="xMidYMax slice" />
-        </View>
       </View>
       <View className="px-6 w-full max-w-lg mx-auto">
         <View className="flex-row justify-between items-center py-20">
